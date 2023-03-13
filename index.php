17457f6afde71821d86b3087df1ef280b97fe250
Brian Huang <zbrianhuang1@gmail.com>
2023-03-13 11:24:22 -0400

    initial

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			<a href="#">Log In</a>
+		</nav>
+	</header>
+	<main>
+		<article>
+			<h2>Welcome to my website</h2>
+			<p>This is some sample text for the main content area.</p>
+		</article>
+		<article>
+			<h2>About me</h2>
+			<p>Here's some information about me.</p>
+		</article>
+		<article>
+			<h2>Contact me</h2>
+			<p>Feel free to contact me using the form below.</p>
+		</article>
+	</main>
+	<footer>
+		<p>&copy; 2023 My Website. All rights reserved.</p>
+	</footer>
+</body>
+</html>
