6b3dbbd7566330b84a684de8f5d9c90dfeaef158
Brian Huang <zbrianhuang1@gmail.com>
2023-03-21 20:34:55 -0400

    :D

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+TODO
+-create tags for related things
+-try except everything.
+-update names json, reduce total names while maintaining a healthy gender ratio
+-improve image search so that images are more relavant to the article content/title
+-SEO optmization: more internal links, more coherent titles
+-fix formatting so that there are no replacement characters in html -i think something something utf 8 not iso-~~~~
+3-20-23 MOSTLY DONE -automate by creating a list of json 
+3-20-23 MOSTLY DONE -make text section a little less redundant ( stop restating topic), make it more relatvant with past info) 
+'''
+from pmain import pmain
+pmain(5,[])
--- pmain.py
+++ pmain.py
@@ -1,11 +1,4 @@
-'''
-TODO
--update names json, reduce total names while maintaining a healthy gender ratio
--improve image search so that images are more relavant to the article content/title
--fix formatting so that there are no replacement characters in html -i think something something utf 8 not iso-~~~~
-3-20-23 MOSTLY DONE -automate by creating a list of json 
-3-20-23 MOSTLY DONE -make text section a little less redundant ( stop restating topic), make it more relatvant with past info) 
-'''
+
 import json
 import time
 from pweb_template import createHtmlFile
@@ -16,37 +9,41 @@ def pmain(attributeNum,topics_list):
     with open("topics.json") as f:
         topicDict = json.loads(f.read())
     total_topic_count = len(topicDict["Topics"])
+    failure_count = 0
     for i in range(total_topic_count):
         
         print("Generating Topic #"+str(i+1)+" out of " +str(total_topic_count)+" topics.")
         current_topic = topicDict["Topics"][i]["title"]
-    
-        start_time = time.perf_counter()
-        #topicList=["How to create intricate wire-wrapped bonsai trees"]
-        #attributesList=[attributeNum]
-        getWebsiteJson(current_topic,attributeNum)
-        json_data=""
-        with open('content.json') as json_file:
-            json_data = json_file.read()
-            content= json.loads(json_data)
-            json_file.close()
+        try:
+            start_time = time.perf_counter()
+            #topicList=["How to create intricate wire-wrapped bonsai trees"]
+            #attributesList=[attributeNum]
+            getWebsiteJson(current_topic,attributeNum)
+            json_data=""
+            with open('content.json') as json_file:
+                json_data = json_file.read()
+                content= json.loads(json_data)
+                json_file.close()
 
 
-        for objects in content:
+            for objects in content:
 
-            createHtmlFile(content[objects])
+                createHtmlFile(content[objects])
 
-        with open("log.txt", 'a') as f:
-            f.write(json.dumps(content))
-            f.write("\n")
-            f.close()
-        with open('content.json', 'w') as f:
-            print("data in 'content.json' successfully deleted")
-            f.write("{}")
+            with open("log.txt", 'a') as f:
+                f.write(json.dumps(content))
+                f.write("\n")
+                f.close()
+            with open('content.json', 'w') as f:
+                print("data in 'content.json' successfully deleted")
+                f.write("{}")
 
-        end_time=time.perf_counter()
-        print(f"Completed in {end_time-start_time:0.4f} seconds")
+            end_time=time.perf_counter()
+            print(f"Completed in {end_time-start_time:0.4f} seconds")
+        except:
+            print("Failed to create "+current_topic+". Moving on...")
+            failure_count+=1
     total_end_time=time.perf_counter()
-    print(str(total_topic_count)+" pages with "+str(attributeNum)+f" attributes completed in {total_end_time-total_start_time:0.4f} seconds")
+    print("\n\n\nFailures:"+str(failure_count))
+    print(str(total_topic_count-failure_count)+"/"+str(total_topic_count)+" pages with "+str(attributeNum)+f" attributes completed in {total_end_time-total_start_time:0.4f} seconds")
     
-pmain(5,[])
